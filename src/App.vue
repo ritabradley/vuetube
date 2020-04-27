3bc4e719aa3eeb2ac82893430de0f41db1477cd6
Rita Bradley <contact@ritabradley.com>
2020-04-27 16:31:14 -0700

    Remove useless files and setup tailwind
    - also rendered input for SearchBar component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <SearchBar msg="Welcome to Your Vue.js App" />
+    <div>
+        <SearchBar></SearchBar>
     </div>
 </template>
 
 <script>
-import SearchBar from './components/SearchBar.vue';
+    import SearchBar from './components/SearchBar';
 
-export default {
-    name: 'App',
-    components: {
-        SearchBar,
-    },
-};
+    export default {
+        name: 'App',
+        components: {
+            SearchBar,
+        },
+    };
 </script>
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
+<style></style>
